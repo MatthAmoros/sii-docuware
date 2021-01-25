6ac2f602452c1f72693a052af59b4d766c7edf7b
MatthAmoros <matth.amoros@gmail.com>
2021-01-25 12:05:47 -0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-DOCUWARE_USERNAME = 'Matthieu AMOROS'
-DOCUWARE_PASSWORD = 'Andinexia2020*'
-DOCUWARE_ENDPOINT = 'https://verger-blottiere.docuware.cloud/DocuWare/Platform'
-DOCUWARE_ORGANIZATION = 'Groupe La Blottiere'
